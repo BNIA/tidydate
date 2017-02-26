647d1342132e96a3fc4513a24011c24af408c309
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-02-25 21:28:22 -0500

    refactored tidydate from tidystar

--- tests/test_tidydate.py
+++ tests/test_tidydate.py
@@ -10,18 +10,23 @@ from __future__ import absolute_import, print_function, unicode_literals
 from os import path
 
 from .context import modules  # pylint: disable=unused-import
-from modules import tidydate
+from modules import tidystar
 
 SAMPLE_DIR = "samples"
-FILE_CSV = path.join(SAMPLE_DIR, "test_csv.csv")
-FILE_XLSX = path.join(SAMPLE_DIR, "test_xlsx.xlsx")
+FILE_CSV = path.join(SAMPLE_DIR, "test.csv")
+# FILE_XLSX = path.join(SAMPLE_DIR, "test_xlsx.xlsx")
 
-VALID_COL = "Recv_Date"
+VALID_COL = {
+    "date": "Recv_Date",
+    "block": "Block",
+    "lot": "Lot",
+    "blocknlot": "Block_Lot"
+}
 INVALID_COL = "Case_Nbr"
 DNE_COL = "Meaning of life"
 
-csv_obj = tidydate.TidyDate(FILE_CSV, debug=True)
-xlsx_obj = tidydate.TidyDate(FILE_XLSX, debug=True)
+csv_obj = tidystar.TidyStar(FILE_CSV, debug=True)
+# xlsx_obj = tidystar.TidyStar(FILE_XLSX, debug=True)
 
 
 def test_csv_valid():
@@ -32,35 +37,35 @@ def test_csv_valid():
     assert(csv_obj.download())
 
 
-def test_csv_invalid():
-    """CSV file with invalid column selected"""
+# def test_csv_invalid():
+#     """CSV file with invalid column selected"""
 
-    csv_obj.set_col(INVALID_COL)
+#     csv_obj.set_col(INVALID_COL)
 
-    assert(not csv_obj.download())
-
-
-def test_xlsx_valid():
-    """Excel file with valid column selected"""
-
-    xlsx_obj.set_col(VALID_COL)
-
-    assert(xlsx_obj.download())
+#     assert(not csv_obj.download())
 
 
-def test_xlsx_invalid():
-    """Excel file with invalid column selected"""
+# def test_xlsx_valid():
+#     """Excel file with valid column selected"""
 
-    xlsx_obj.set_col(INVALID_COL)
+#     xlsx_obj.set_col(VALID_COL)
 
-    assert(not xlsx_obj.download())
+#     assert(xlsx_obj.download())
 
 
-def test_xlsx_missing():
-    """Excel file with non-existent column selected"""
+# def test_xlsx_invalid():
+#     """Excel file with invalid column selected"""
 
-    try:
-        xlsx_obj.set_col(DNE_COL)
+#     xlsx_obj.set_col(INVALID_COL)
 
-    except SystemExit:
-        assert(not xlsx_obj.download())
+#     assert(not xlsx_obj.download())
+
+
+# def test_xlsx_missing():
+#     """Excel file with non-existent column selected"""
+
+#     try:
+#         xlsx_obj.set_col(DNE_COL)
+
+#     except SystemExit:
+#         assert(not xlsx_obj.download())
---
+++ tidydate/modules/tidyer.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""tidyer
+
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+import sys
+
+from dateutil import parser as date_parser
+import numpy as np
+import pandas as pd
+
+from .settings import TIDY_DATE_SPLIT
+
+
+class TidyDate(object):
+
+    def __init__(self, df, column):
+
+        self.df = df
+        self.date_col = column["date"]
+
+    @staticmethod
+    def parse_date(date_str):
+
+        try:
+            return date_parser.parse(date_str)
+
+        except (TypeError, ValueError):
+
+            try:
+
+                def split_date():
+
+                    return (date_str[-4:], date_str[:-6], date_str[-6:-4])
+
+                return date_parser.parse('-'.join(split_date()))
+
+            except (TypeError, ValueError):
+                return None
+
+    def clean_date_col(self):
+        """Parses and standardizes the selected column values
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        if self.date_col:
+
+            if np.dtype(self.df[self.date_col]) == np.dtype("datetime64[ns]"):
+                sys.exit("Column is already in date format")
+
+            self.df["tidy_date"] = self.df[self.date_col].apply(
+                lambda x: self.parse_date(str(x))
+            )
+
+            self.df["tidy_date"] = self.df["tidy_date"].apply(
+                lambda x: x.date() if pd.notnull(x) else x
+            )
+
+    def split_date(self):
+        """Splits the "tidy_date" column into separate tidy year, month and
+        day columns
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        for index, col in enumerate(TIDY_DATE_SPLIT):
+
+            try:
+
+                self.df[col] = self.df["tidy_date"].apply(
+                    lambda x: int(str(x).split("-")[index])
+                    if pd.notnull(x) else x
+                )
+
+            except IndexError:
+                continue
+
+    def fill_na(self):
+        """Fills values that were unable to be parsed with the original values
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        TIDY_DATE_SPLIT.append("tidy_date")
+
+        for col in TIDY_DATE_SPLIT:
+            self.df[col].fillna(self.df[self.date_col], inplace=True)
+
+    def tidydate(self):
+
+        self.clean_date_col()
+        self.split_date()
+        self.fill_na()
+
+        return self.df
+
+
+# class TidyBlockNLot(object):
+
+#     def __init__(self, df, column):
