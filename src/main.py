0c61991d0f6a630d05949fd81e56e93b1018b62e
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-06-04 12:47:02 -0400

    add debug mode to main script

--- src/main.py
+++ src/main.py
@@ -7,12 +7,25 @@ This file serves as the driver for the program by initializing the Flask app
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
+import argparse
 from os import path, makedirs
 import webbrowser
 
 from frontend import views
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="The main script for TidyAll"
+    )
+
+    # debug mode
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="| Debug mode"
+    )
+
+    # parse arguments to pass into function
+    args = parser.parse_args()
 
     port_file_name = "port.txt"
 
@@ -33,4 +46,4 @@ if __name__ == "__main__":
             "http://localhost:{PORT}".format(PORT=port)
         )
 
-        views.app.run(port=int(port), debug=True)
+        views.app.run(port=int(port), debug=bool(args.debug))
